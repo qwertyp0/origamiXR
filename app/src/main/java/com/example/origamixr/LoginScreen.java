6a9725658d09d4a2de6344b002d0f69c8f846642
morrisq <morrisq111@gmail.com>
2019-11-29 16:53:49 -0500

    Basic Splash and Login Needs more work on the UI side of things but they are connected to the main activity Will try to work on the other activity screens

--- app/src/main/java/com/example/origamixr/LoginScreen.java
+++ app/src/main/java/com/example/origamixr/LoginScreen.java
@@ -1,6 +1,10 @@
 package com.example.origamixr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,21 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+    }
+
+    public void signIntoMainActivity(View v)
+    {
+        TextView username = findViewById(R.id.username_field);
+        TextView email = findViewById(R.id.email_field);
+        TextView password = findViewById(R.id.password_field);
+
+        if(username.getText().toString().equals(""))
+            Toast.makeText(getApplicationContext(), "Please provide a valid username", Toast.LENGTH_SHORT).show();
+        else if(email.getText().toString().equals(""))
+            Toast.makeText(getApplicationContext(), "Please provide a valid email", Toast.LENGTH_SHORT).show();
+        else if(password.getText().toString().equals(""))
+            Toast.makeText(getApplicationContext(), "Please provide a valid password", Toast.LENGTH_SHORT).show();
+        else
+            startActivity(new Intent(getApplicationContext(), MainHomeActivity.class));
     }
 }
